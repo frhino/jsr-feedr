3622deac21a579a24e587e563471116d64432564
frhino <josh.freivogel@gmail.com>
2018-07-04 21:51:55 -0500

    formatted time, returned auth or pub, templ lit of popup

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
-/*
-  Please add all Javascript code to this file.
-*/
-console.log("hi");
-var mySrc = "cnn";
-myUrl = `https://newsapi.org/v2/top-headlines?sources=${mySrc}&apiKey=b157a0eb622340e1a6e0ddbcb797e508`;
+var mySrc = "abc-news-au";
+myUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=b157a0eb622340e1a6e0ddbcb797e508`;
 $.ajax({
 //var mySource = `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=b157a0eb622340e1a6e0ddbcb797e508`;
 
@@ -22,22 +18,43 @@ $.ajax({
 
   .done(function(data){
     $("#main").empty();
+    $("#popUp").empty();
     data = data.articles;
     for(i = 0; i < data.length; i++){
 
       var title = data[i].title;
       var author = data[i].author;
-      var date = data[i].publishedAt;
+      var pub = data[i].source.name;
+      var credit;
+        if (author == null || author.match(/http/i)){
+          credit = pub;
+        } else {
+          credit = author + " for " + pub;
+        }
+      //var date = data[i].publishedAt;
+      //console.log(moment(date).format('YYYY MM DD hh:mm:ss:a'));
+      var date = moment(data[i].publishedAt).format('ddd, MMM DD, YYYY hh:mm:ss:a')
       var image = data[i].urlToImage;
-
+      var link = data[i].url;
+      var synop = data[i].description;
+      var popUpTemplate = `
+      <a href="${link}" class="closePopUp">X</a>
+      <div class="container">
+        <h1>${title}</h1>
+        <p>
+          ${synop}
+        </p>
+        <a href=${link} class="popUpAction" target="_blank">Read more from source</a>
+      `
       var articleTemplate = `
       <article class="article">
         <section class="featuredImage">
           <img src="${image}" alt="" />
         </section>
         <section class="articleContent">
-            <a href="#"><h3>${title}</h3></a>
-            <h6>${author}</h6>
+
+            <a href="${link}"><h3>${title}</h3></a>
+            <h6>${credit}</h6>
         </section>
         <section class="impressions">
           ${date}
@@ -47,8 +64,11 @@ $.ajax({
         <div class="clearfix"></div>
       </article>`
       $("#main").append(articleTemplate)
-      console.log(articleTemplate)
-  //    console.log(data)
+//    log the articles ...
+//    console.log(articleTemplate)
+//    console.log(data[i])
+      console.log(credit)
     }
 
   })
+  // <a href="${link}"><h3>${title}</h3></a>
